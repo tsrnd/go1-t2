eb6f352c2cf8a3e87501414057a1bb6fccc08a45
tienphamd <phamdangtien@gmail.com>
2017-11-30 11:32:01 +0700

    show product homepage

--- app/controllers/home.go
+++ app/controllers/home.go
@@ -4,31 +4,38 @@ import (
 	"goweb2/app/models"
 	"goweb2/views"
 	"net/http"
-
 	"goweb2/helper"
-
 	"github.com/julienschmidt/httprouter"
 )
 
+/**
+ * HomeController struct
+ */
 type HomeController struct {
 	helper.Controller
 }
 
 var Homes HomeController
 
+/**
+ * Show home page
+ */
 func (self HomeController) Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
-	val, err := models.AllTests()
+	products, err := models.GetProductLimit()
 	if err != nil {
+
 		return err
 	}
 	compact := map[string]interface{}{
 		"Title": "THIS IS A HOME PAGE!",
-		"Data":  val,
+		"Products":  products,
 	}
+
 	return views.Homes.Index.Render(w, compact)
 }
 
 func (self *HomeController) ReqKey(a helper.Action) httprouter.Handle {
+
 	return httprouter.Handle(func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		if r.FormValue("key") != "topsecret" {
 			http.Error(w, "Invalid key.", http.StatusUnauthorized)
--- app/models/test.go
+++ app/models/test.go
@@ -17,28 +17,31 @@ func init() {
 }
 
 type Test struct {
-	name  string
-	email string
+	Name  string
+	Email string
 }
 
 func AllTests() ([]*Test, error) {
 	rows, err := db.Query("SELECT name, email FROM users")
 	if err != nil {
+		
 		return nil, err
 	}
 	defer rows.Close()
-
 	bks := make([]*Test, 0)
 	for rows.Next() {
 		bk := new(Test)
-		err := rows.Scan(&bk.name, &bk.email)
+		err := rows.Scan(&bk.Name, &bk.Email)
 		if err != nil {
+
 			return nil, err
 		}
 		bks = append(bks, bk)
 	}
 	if err = rows.Err(); err != nil {
+
 		return nil, err
 	}
+
 	return bks, nil
 }
