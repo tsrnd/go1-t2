c17769f4c7add60c5f388a2d9002f4a0867f1b08
Thinh Nguyen V <thinh.nguyen@asiantech.vn>
2017-12-04 16:38:03 +0700

    Create login

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"goweb2/app/models"
 	"goweb2/helper"
 	"goweb2/views"
 	"net/http"
+
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -37,9 +39,14 @@ func (self UserController) Store(w http.ResponseWriter, r *http.Request, ps http
 }
 
 func (self UserController) LoginPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+
+	flashData := helper.GetFlash("auth_err_msg", w, r)
+
 	compact := map[string]interface{}{
-		"Title": "Login",
+		"Title":     "Login",
+		"FlashData": flashData,
 	}
+	fmt.Println(flashData)
 	return views.User.Login.Render(w, compact)
 }
 
@@ -50,6 +57,13 @@ func (self UserController) ShowContactPage(w http.ResponseWriter, r *http.Reques
 	return views.User.Contact.Render(w, compact)
 }
 
-// func (self UserController) Login(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
-// 	ok, errMsg := models.Login(r)
-// }
+func (self UserController) Login(w http.ResponseWriter, r *http.Request, ps httprouter.Params) (a error) {
+	if ok, errMsg := models.Login(w, r); ok == false {
+		helper.SetFlash(errMsg, w, r)
+		http.Redirect(w, r, "/login", 302)
+		return a
+	}
+
+	http.Redirect(w, r, "/", 302)
+	return a
+}
