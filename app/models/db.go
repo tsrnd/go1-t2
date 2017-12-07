f390ac8c9ff7a7f7f6616e0554a11e056d5e3892
Thinh Nguyen V <thinh.nguyen@asiantech.vn>
2017-12-07 16:02:21 +0700

    fix heroku

--- app/models/db.go
+++ app/models/db.go
@@ -23,8 +23,8 @@ func ConnectDB() {
 	}
 	//dsn
 	dsn := fmt.Sprintf("host='%s' port='%d'  user='%s' password='%s' dbname=%s sslmode=%s", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"), sslmode)
-	dsn = "host=localhost port=5432 user=thinhnguyenv. " +
-		"password='' dbname=golang2 sslmode=disable"
+	dsn = "host=ec2-107-20-176-7.compute-1.amazonaws.com port=5432 user=csjzeohasarmuf" +
+		"password='269122355cad43ad45ce1131f80ec194b4051ba20cde0d51cc94912d0e907ff2' dbname=d63dcu920h728n"
 	//open a database connection
 	db, err = gorm.Open("postgres", dsn)
 
