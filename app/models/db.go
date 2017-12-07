a895ec56532de3eb9aa765a5341c72dfa1dc3375
Thinh Nguyen V <thinh.nguyen@asiantech.vn>
2017-12-07 15:55:53 +0700

    fix connect db

--- app/models/db.go
+++ app/models/db.go
@@ -12,17 +12,17 @@ var db *gorm.DB
 
 func ConnectDB() {
 	var err error
-	address := ""
 	port := "5432"
 	val, ok := os.LookupEnv("DB_PORT")
 	if ok {
 		port = val
 	}
-	if host, ok := os.LookupEnv("DB_HOST"); ok && host != "" {
-		address = fmt.Sprintf("host=%s port=%d ", host, port)
+	sslmode := "true"
+	if disable, check := os.LookupEnv("DB_SSLMODE"); check {
+		sslmode = disable
 	}
 	//dsn
-	dsn := fmt.Sprintf("%s user='%s' password='%s' dbname=%s sslmode=disable", address, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"))
+	dsn := fmt.Sprintf("host='%s' port='%d'  user='%s' password='%s' dbname=%s sslmode=%s", os.Getenv("DB_HOST"), port, os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_DATABASE"), sslmode)
 	dsn = "host=localhost port=5432 user=thinhnguyenv. " +
 		"password='' dbname=golang2 sslmode=disable"
 	//open a database connection
