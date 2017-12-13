f6c5fd094090467614606dd590568d2df4cae71e
Tien Pham D <tien.pham@asiantech.vn>
2017-12-13 09:20:39 +0700

    fix model db

--- app/controllers/admin/product.go
+++ app/controllers/admin/product.go
@@ -6,6 +6,7 @@ import (
 	"goweb2/views/viewAdmin/product"
 	"net/http"
 	"github.com/julienschmidt/httprouter"
+	"regexp"
 )
 
 type ProductAdminController struct {
@@ -26,4 +27,25 @@ func (self ProductAdminController) Index(w http.ResponseWriter, r *http.Request,
 	}
 	
 	return product.ProductAdmin.Index.Render(w, r, compact)
+}
+
+func (self ProductAdminController) Delete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+	reg, _ := regexp.Compile(`\d+$`)
+	productId := reg.FindString(r.URL.Path)
+	admin.DeleteProductById(productId)
+	compact := map[string]interface{}{
+		"Title":    "THIS IS A HOME PAGE!",
+		"Url":      helper.BaseUrl(),
+	}
+	
+	return product.ProductAdmin.Index.Render(w, r, compact)
+}
+
+func (self ProductAdminController) ShowFormStore(w http.ResponseWriter, r *http.Request, ps httprouter.Params) error {
+	compact := map[string]interface{}{
+		"Title":    "THIS IS A HOME PAGE!",
+		"Url":      helper.BaseUrl(),
+	}
+	
+	return product.FormAddProductAdmin.Index.Render(w, r, compact)
 }
